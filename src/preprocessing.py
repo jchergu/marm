9f2cd7cc46c9a3634c43bc325653eb84ed4eaf03
Jacopo Chergui <jackchergui09@gmail.com>
2025-12-03 15:45:23 +0300

    update: preprocessing.py + readme#nextOps

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,6 +7,10 @@ def preprocess(df):
     # remove first column (it's an index)
     df = df.iloc[:, 1:]
 
+    # columns to remove
+    drop_cols = {"track_id", "track_name", "album_name", "artists", "popularity", "explicit", "track_genre"}
+    df = df.drop(columns=[c for c in drop_cols if c in df.columns])
+
     # Handle missing values
     df = df.dropna()
 
